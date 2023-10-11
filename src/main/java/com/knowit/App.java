e94db52c4fd817c1fa05b8c4f141e3ac50b8ab23
Anh <qapham@stud.ntnu.no>
2023-10-11 13:15:11 +0200

    Fix mvn test commando and tests

--- src/main/java/com/knowit/App.java
+++ src/main/java/com/knowit/App.java
@@ -1,11 +1,12 @@
 package com.knowit;
 
 /**
- * Hello world!
+ * This is the App.java file that runs the application.
  *
  */
-public class App 
-{
+public class App {
+
+    
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
